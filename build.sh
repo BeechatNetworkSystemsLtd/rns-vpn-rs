891946f7d42705c7ba29734acb3dc10bdd911f7a
Shane Pearman <spearman@gitlab.com>
2026-01-20 00:34:21 +0100

    add build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+. /opt/yocto-sdk/environment-setup-cortexa7t2hf-neon-vfpv4-ostl-linux-gnueabi
+rustup target add armv7-unknown-linux-gnueabihf
+cargo build
+
+exit 0
--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@
 set -e
 set -x
 
-cargo build
+cargo build --release
 RNS_VPN_PRIVKEY_PATH="./privkey.pem" \
 RNS_VPN_SIGNKEY_PATH="./signkey.pem" \
 RUST_LOG=debug RUST_BACKTRACE=1 \
-  sudo -E target/debug/rns-vpn -p 4242 -f 127.0.0.1:4243 #-i "vpn-test-client"
+  sudo -E target/release/rns-vpn -p 4242 -f 192.168.1.129:4243 #-i "vpn-test-client"
 
 exit 0
